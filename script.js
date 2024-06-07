7804b20ef8eff469f4be7abad7d06f77a2077081
Scott Fennell <slfennell01@gmail.com>
2024-06-07 12:52:11 -0400

    Finalize feedback display functionality.

--- script.js
+++ script.js
@@ -10,9 +10,20 @@ document.addEventListener('DOMContentLoaded', function(){
         
     
         // varibles for elements of feedback form
-        const firstName = document.addEventListener('firstName').value;
-        const lastName = document.addEventListener('lastName').value;
+        const firstName = document.addEventListener('firstName').value.trim();
+        const lastName = document.addEventListener('lastName').value.trim();
         const ratingFB = document.addEventListener('ratiingFB').value;
         const comments = document.addEventListener('comments').value;
-    })
-})
+
+
+        // Finalize feedback display functionality
+        document.getElementById('fbSubmission').innerHTML = 
+        `<h4>Feedback submitted successfully!</h4>
+        <p><strong>First name: </strong> ${firstName}</p>
+        <p><strong>Last name: </strong> ${lastName}</p>
+        <p><strong>Rating: </strong> ${ratingFB}</p>
+        <p><strong>Comments:</strong> ${comments}</p>
+        `
+        document.getElementById('fbSumission').reset();
+    });
+});
