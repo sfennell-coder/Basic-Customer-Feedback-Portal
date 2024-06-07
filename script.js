a40f15203818c8c4402f4e973c6317788d5320bb
Scott Fennell <slfennell01@gmail.com>
2024-06-07 13:47:30 -0400

    Finalize feedback display functionality
    Finalize feedback display functionality. Fixed issues throughout code.

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // U86364709
 
 // Implement form submission and validation
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.addEventListener('feedbackForm');
-    const fbSubmission = document.addEventListener('fbSumission');
+document.getElementById("feedbackForm").addEventListener("submit", (e)=>{
+    e.preventDefault(); // Anti reload or refresh of screen
 
-    form.addEventListener('submit', function(e){
-        e.preventDefault(); //Anti reload or refresh
-        
-    
-        // varibles for elements of feedback form
-        const firstName = document.addEventListener('firstName').value.trim();
-        const lastName = document.addEventListener('lastName').value.trim();
-        const ratingFB = document.addEventListener('ratiingFB').value;
-        const comments = document.addEventListener('comments').value;
+    // varibles for elements of feedback form
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const ratingFB = document.getElementById("ratingFB").value;
+    const comments = document.getElementById("comments").value;
 
+    // if statement alert output
+   if(!firstName || !lastName || !ratingFB || !comments){
+    alert('Opps! You may not have finished all required fields. Please fill out all fields.');
+    return;
+   }
+   
 
-        // Finalize feedback display functionality
-        document.getElementById('fbSubmission').innerHTML = 
-        `<h4>Feedback submitted successfully!</h4>
-        <p><strong>First name: </strong> ${firstName}</p>
-        <p><strong>Last name: </strong> ${lastName}</p>
-        <p><strong>Rating: </strong> ${ratingFB}</p>
-        <p><strong>Comments:</strong> ${comments}</p>
-        `
-        document.getElementById('fbSumission').reset();
-    });
+    // Finalize feedback display functionality
+    document.getElementById("fbSubmission").innerHTML = `
+    <h3>Feedback submitted successfully!</h3><br>
+    <p><strong>First name: </strong> ${firstName}</p>
+    <p><strong>Last name: </strong> ${lastName}</p>
+    <p><strong>Rating: </strong> ${ratingFB}</p>
+    <p><strong>Comments:</strong> ${comments}</p>
+    `
+    document.getElementById("feedbackForm").reset();
 });
